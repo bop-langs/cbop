9e32f2df54ca717d0ae4cbb0a696b01bfefa051a
Ben O'Halloran <bohallor@u.rochester.edu>
2016-04-11 23:32:54 -0400

    Revert "synchronize bop_msg"
    This reverts commit 1d35956aad0a1a4907185ce039bcb0ab4385c06e.

--- utils.c
+++ utils.c
@@ -5,9 +5,7 @@
 #include <sys/time.h>
 #include <unistd.h>
 #include <semaphore.h>
-#include <pthread.h>
 #include <fcntl.h>
-#include <sys/mman.h>
 #include "bop_api.h"
 #include "utils.h"
 
@@ -17,7 +15,7 @@ extern int ppr_index;
 extern int spec_order;
 extern bop_mode_t bop_mode;
 
-static pthread_mutex_t * msg_lock;
+sem_t *bopmsg_sem = NULL;
 
 task_status_t BOP_task_status(void) {
   return task_status;
@@ -64,9 +62,8 @@ char *strerror(int errnum);
 void bop_msg(int level, const char * msg, ...) {
  if(bop_verbose >= level)
   {
-    msg_init();
-    if(msg_lock != NULL)
-      pthread_mutex_lock(msg_lock);
+      //msg_init();
+    sem_wait(bopmsg_sem);
     va_list v;
     va_start(v,msg);
     fprintf(stderr, "%d-", getpid());
@@ -101,23 +98,23 @@ void bop_msg(int level, const char * msg, ...) {
     vfprintf(stderr,msg,v);
     fprintf(stderr,"\n");
     fflush(stderr);
-    if(msg_lock != NULL)
-      pthread_mutex_unlock(msg_lock);
+    sem_post(bopmsg_sem);
   }
 }
 
 void msg_init(){
-  if (!msg_lock){
-      msg_lock = mmap(NULL, sizeof(pthread_mutex_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
-      pthread_mutexattr_t mutexAttr;
-      if(pthread_mutexattr_init(&mutexAttr)) perror("Couldn't create mutex attr");
-      if(pthread_mutexattr_setpshared(&mutexAttr, PTHREAD_PROCESS_SHARED)) perror("Error sharing mutex");
-      if(pthread_mutex_init(msg_lock, &mutexAttr)) perror("Error creating shared msg mutex");
+  if (!bopmsg_sem){
+      bopmsg_sem = sem_open("/bopmsg.sem", (O_CREAT), S_IRWXO|S_IRWXU|S_IRWXG, 0);
+      if(bopmsg_sem == SEM_FAILED){
+          printf("Error in BOP_Init: %s\n", strerror(errno));
+      }
+      assert(bopmsg_sem != NULL);
+      assert(bopmsg_sem != SEM_FAILED);
+      sem_post(bopmsg_sem);
   }
 }
 void msg_destroy(){
-  munmap(msg_lock, 0);
-  pthread_mutex_destroy(msg_lock);
+  sem_close(bopmsg_sem);
 }
 
 /* read the environment variable env, and returns it's integer value.
